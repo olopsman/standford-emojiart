8a04308a6a1ceb053c431102f7a4a7f3cd166a71
Paulo Orquillo <paulo.orquillo@gmail.com>
2021-08-15 12:30:23 +1200

    Initial Commit

---
+++ EmojiArtDemo/EmojiArtDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtDemoApp.swift
+//  EmojiArtDemo
+//
+//  Created by Paulo Orquillo on 15/08/21.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
